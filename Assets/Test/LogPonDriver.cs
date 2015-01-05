43feef1affd0744bb4ce2f545c34ee5d810a7b28
satoshi watanabe <watanabe_satoshi@cyberagent.co.jp>
2015-01-05 22:29:17 +0900

    ざっくり実験コード

---
+++ Assets/Test/LogPonDriver.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using LogPon;
+
+public class LogPonDriver : MonoBehaviour
+{
+    IEnumerator Start ()
+    {
+        LogEventDistributor.Init (false);
+        var isValid = LogEventDistributor.AddLogCallback (LogPonAdapter.AddLog);
+        Debug.Log (isValid);
+
+        while (true) {
+            Debug.Log (Time.frameCount);
+            yield return new WaitForSeconds (2f);
+        }
+    }
+}
